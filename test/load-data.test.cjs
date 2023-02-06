adf8c31f43ca1d419958b2357998da9fa49c3ed6
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2023-02-06 03:23:24 -0800

    check if loadData returns an array without an argument

---
+++ test/load-data.test.cjs
@@ -0,0 +1,22 @@
+var {auth,loadData} = require('@codingnninja/sapabase');
+var expect = require('chai').expect;
+var {Octokit} = require('@octokit/rest');
+
+describe('Loading data from a Github repo', function() {
+
+    context('Loading data without an argument', function() {
+        it('should return the items in the latest file', async function() {
+            this.timeout(3000)
+            auth({
+                username: 'codingnninja',
+                reponame: 'bloguard',
+                folder: 'user',
+                Octokit
+            })  
+            const actual = await loadData();
+            // const expected = await loadData(1);       
+            expect(actual).to.be.an('array');
+        })
+    })
+
+})
